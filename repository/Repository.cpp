52f89535d8873f03dce1f675464a2c6035ef7a67
AbdulrahmanSalah <sa8640205@gmail.com>
2023-11-29 12:48:38 +0200

    create repo(student,course,teacher

---
+++ repository/Repository.cpp
@@ -0,0 +1,37 @@
+#include "../model/models.cpp"
+#include <bits/stdc++.h>
+using namespace std;
+
+/////////////////////////////  Student \\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+// Interface studentrepository
+class Studentrepository
+{
+};
+
+// Class StudentRepositoryImpl
+class StudentrepositoryImpl : public Studentrepository
+{
+};
+
+/////////////////////////////  Course \\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+
+// Interface CourseRepository
+class CourseRepository
+{
+};
+// Class CourseRepositoryImpl
+class CourseRepositoryImpl : public CourseRepository
+{
+};
+
+/////////////////////////////  Teacher \\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+// Interface TeacherRepository
+class TeacherRepository
+{
+};
+// Class TeacherRepositoryImpl
+class TeacherRepositoryImpl : public TeacherRepository
+{
+};
